6d661741c36e34bea201a9c501b933db61061ff0
Alitariq747 <alitariq747@outlook.com>
2023-10-12 09:38:20 +0500

    Update route.ts

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,10 +1,10 @@
 import { client } from "@/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 import { getAuth } from "@clerk/nextjs/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const { userId } = getAuth(req);
 
